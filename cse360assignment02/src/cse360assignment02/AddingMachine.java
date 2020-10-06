b70c5c8c3b1ad313665eac6be4c2c4f605472fbf
martinituartehernandez <martinituartehernandez@martins-mbp-2.3521.dhcp.asu.edu>
2020-10-06 11:37:04 -0700

    Intial code

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,55 @@
+package cse360assignment02;
+
+public class  AddingMachine {
+	
+  private int total;
+
+  /*
+  * Description: 
+  *@param (name of param and *description of it)
+  *@return (name of what gets returned *and what it is)
+  */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  /*
+   * Description: gets the final int of the calculations 
+   * @param no paramters 
+   * @return an int of the final total
+   */
+  public int getTotal () {
+    return 0;
+  }
+  /*
+   * Description:  adds an int
+   * @param int that will be added
+   * @return no return
+   */
+  public void add (int value) {
+	
+  }
+  /*
+   * Description: subtracts an int
+   * @param int that will be subtracted 
+   * @return no return
+   */
+  public void subtract (int value) {
+	
+  }
+  /*
+   * Description: displays the calculations 
+   *@param no parameters
+   *@return  the calculatations as a string
+   */
+  public String toString () {
+    return "";
+  }
+  /*
+   * Description: clears the total
+   * @param no parameters
+   * @return no return
+   */
+  public void clear() {
+	 
+  }
+}
