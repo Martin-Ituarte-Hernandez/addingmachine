dacfb98444eb1b53e4cfafe4f976e2ee0af33bf9
martinituartehernandez <martinituartehernandez@192.168.1.5>
2020-10-06 14:15:34 -0700

    changed code.

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,63 +1,55 @@
-package cse360assignment01;
+package cse360assignment02;
 
-public class AddingMachine {
-	  
-		  private int total;
-		  private String calculations;
-		  /*
-		  * Description: 
-		  *@param (name of param and *description of it)
-		  *@return (name of what gets returned *and what it is)
-		  */
-		  public AddingMachine () {
-		    total = 0;  // not needed - included for clarity
-		  }
-		  /*
-		   * Description: gets the final int of the calculations 
-		   * @param no paramters 
-		   * @return an int of the final total
-		   */
-		  public int getTotal () {
-		    return total;
-		  }
-		  /*
-		   * Description:  adds an int
-		   * @param int that will be added
-		   * @return no return
-		   */
-		  public void add (int value) {
-			  total+=value;
-			  calculations= calculations+"+"+value;
-		  }
-		  /*
-		   * Description: subtracts an int
-		   * @param int that will be subtracted 
-		   * @return no return
-		   */
-		  public void subtract (int value) {
-			  total-=value;
-			  calculations= calculations+"-"+value;
-		  }
-		  /*
-		   * Description: displays the calculations 
-		   *@param no parameters
-		   *@return  the calculatations as a string
-		   */
-		  public String toString () {
-		    return calculations+"="+total;
-		  }
-		  /*
-		   * Description: clears the total
-		   * @param no parameters
-		   * @return no return
-		   */
-		  public void clear() {
-			  total=0; calculations="";
-		  }
-		}
+public class main AddingMachine {
+	  public static void main(String[] args) {
+  private int total;
 
-		  
-		  
-		  
-		  
-		  
+  /*
+  * Description: 
+  *@param (name of param and *description of it)
+  *@return (name of what gets returned *and what it is)
+  */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }}
+  /*
+   * Description: gets the final int of the calculations 
+   * @param no paramters 
+   * @return an int of the final total
+   */
+  public int getTotal () {
+    return 0;
+  }
+  /*
+   * Description:  adds an int
+   * @param int that will be added
+   * @return no return
+   */
+  public void add (int value) {
+	
+  }
+  /*
+   * Description: subtracts an int
+   * @param int that will be subtracted 
+   * @return no return
+   */
+  public void subtract (int value) {
+	
+  }
+  /*
+   * Description: displays the calculations 
+   *@param no parameters
+   *@return  the calculatations as a string
+   */
+  public String toString () {
+    return "";
+  }
+  /*
+   * Description: clears the total
+   * @param no parameters
+   * @return no return
+   */
+  public void clear() {
+	 
+  }
+}
